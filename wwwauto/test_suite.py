2b86834340c9da483fcfe380ffd31d9acea1a26c
kang <slash@zigbang.com>
2018-09-13 18:03:40 +0900

    추천 상품 위시리스트에 담기 기능 추가

--- wwwauto/test_suite.py
+++ wwwauto/test_suite.py
@@ -1,3 +1,3 @@
-from wwwauto.web_00_payment import PaymentTest
+# from wwwauto.web_00_payment import PaymentTest
 # from wwwauto.web_01_search import SearchTest
-# from wwwauto.web_02_cities_detail import CitiesDetailTest
+from wwwauto.web_02_cities_detail import CitiesDetailTest
--- wwwauto/web_02_cities_detail.py
+++ wwwauto/web_02_cities_detail.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
 import unittest
 import os
 import json
@@ -51,7 +52,27 @@ class CitiesDetailTest(unittest.TestCase):
 
         self.driver.maximize_window()
 
-        #TODO 2. 인기 여행지 선택
+        #TODO 2. 계정 로그인
+
+        self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "gtm-gnb-signin"))).click()
+
+        self.wait.until(EC.visibility_of_element_located((By.NAME, "user[email]"))).send_keys(email)
+
+        self.wait.until(EC.visibility_of_element_located((By.NAME, "user[password]"))).send_keys(password)
+
+        self.wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "btn-wrap")))[2].click()
+
+        self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "profile-photo "))).click()
+
+        userName = self.wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, ".gnb-popup-menu__item--profile > .text"))).text
+
+        if not userName == name:
+            raise Exception("로그인 계정 정보가 올바르지 않습니다.")
+
+        self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "profile-photo "))).click()
+
+        #TODO 3. 인기 여행지 선택
 
         popular_city_section = self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'mrt-popular-city')))
 
@@ -60,8 +81,7 @@ class CitiesDetailTest(unittest.TestCase):
         popular_city_hover.perform()
         time.sleep(1)
 
-        rnd_index = random.choice(range(14))
-        print(rnd_index)
+        rnd_index = random.choice(range(13))
 
         cities_rnd_choice = self.wait.until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'popular-city__item--name')))[rnd_index].text
         # 랜덤으로 선택한 인기 여행지 이름
@@ -75,7 +95,7 @@ class CitiesDetailTest(unittest.TestCase):
         if not cities_rnd_choice == cities_detail_title:
             raise Exception('선택한 인기 여행지와 노출되는 상세화면 여행지가 상이합니다.', cities_rnd_choice, cities_detail_title)
 
-        # TODO 3. 여행일정 등록
+        # TODO 4. 여행일정 등록
 
         self.wait.until(EC.visibility_of_element_located((By.CLASS_NAME, 'calendar-container__input'))).click()
         # 여행일정 등록 버튼 클릭
@@ -89,8 +109,23 @@ class CitiesDetailTest(unittest.TestCase):
         self.wait.until(EC.visibility_of_element_located((By.XPATH, "//td[@data-handler='selectDay']/a[text()="+"'"+str(calendar_2nights_3days)+"'"+"]"))).click()
         # 여행일정 당일+3 선택
 
-        time.sleep(10)
+        # TODO 5. 추천 상품 위시리스트에 담기
 
+        wish = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.swiper-slide-active > .wish')))
+        # 위시리스트 버튼 엘리먼트
+
+        try:
+            self.driver.find_element_by_class_name('wished')
+            # 위시리스트 설정이 활성화된 상품인 경우
+
+        except NoSuchElementException:
+            # 위시리스트 미설정 상태
+
+            wish.click()
+            # 첫번째 추천 상품 위시리스트 추가
+
+
+        time.sleep(3)
 
     def tearDown(self):
         self.driver.quit()
